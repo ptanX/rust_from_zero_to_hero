d49bc8ada8f26c722119ceda53573ce038a651bb
ptanX <nguyenthaibinhbk@gmail.com>
2024-06-03 01:07:36 +0700

    adding struct definition method and instantiate method

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,29 @@
-mod greeting;
-mod dynamic_dispatch;
-mod guess;
-mod struct_example;
-
-use std::cmp::Ordering;
-use crate::dynamic_dispatch::object_trait::{Animal, Cat, Dog};
-use std::io;
 use rand::Rng;
 
+use crate::dynamic_dispatch::object_trait::{Animal, Cat, Dog};
+use crate::struct_learning::basic_structure::User;
+
+mod greeting;
+mod dynamic_dispatch;
+mod struct_example;
+mod struct_learning;
+
 static D: fn(i32) -> i32 = |i: i32| -> i32 {
-    i*i
+    i * i
 };
-fn create_animal(kind: &str) -> Box<dyn Animal> {
-    if kind == "DOG" {
-        Box::new(Dog)
-    } else {
-        Box::new(Cat)
-    }
-}
 
 fn main() {
-    let mut s = String::from("hello");
-    s.push_str(", world!"); // push_str() appends a literal to a String
-    println!("{}", s); // This will print `hello, world!`
+    // For struct learning
+    let first_user: User = User {
+        active: true,
+        username: String::from("some username123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+    let user_email = first_user.email;
+    println!("first user email is: {}", user_email);
+    println!("is user active: {}", first_user.active);
+    println!("first username is: {}", first_user.username);
+    println!("first sign_in_count is: {}", first_user.sign_in_count);
 }
 
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
