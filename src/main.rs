9f44dbadf02cf02fe229a7ffbb327a1a7a8d3deb
ptanX <nguyenthaibinhbk@gmail.com>
2024-06-03 23:02:14 +0700

    adding basic enum definition into repository

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use rand::Rng;
 
-use crate::dynamic_dispatch::object_trait::{Animal, Cat, Dog};
+use crate::dynamic_dispatch::object_trait::Animal;
+use crate::enum_learning::ip_addr_enum::{create_ip_addr_with_string, get_ip_addr_with_string_value};
 use crate::struct_learning::basic_structure::User;
 
 mod greeting;
 mod dynamic_dispatch;
-mod struct_example;
 mod struct_learning;
+mod enum_learning;
 
 static D: fn(i32) -> i32 = |i: i32| -> i32 {
     i * i
@@ -14,6 +15,7 @@ static D: fn(i32) -> i32 = |i: i32| -> i32 {
 
 fn main() {
     // For struct learning
+    println!("################### Struct Learning ###################");
     let first_user: User = User {
         active: true,
         username: String::from("some username123"),
@@ -25,5 +27,14 @@ fn main() {
     println!("is user active: {}", first_user.active);
     println!("first username is: {}", first_user.username);
     println!("first sign_in_count is: {}", first_user.sign_in_count);
+    // for enum learning
+    println!("################### Enum Learning ###################");
+    let ip_kind = String::from("V4");
+    let ip_v4_value = String::from("127.0.0.1");
+    let ip_addr_value = create_ip_addr_with_string(&ip_kind, &ip_v4_value);
+    let ip_value = get_ip_addr_with_string_value(&ip_addr_value);
+    println!("ip value is: {}", ip_value)
 }
+
+
 
